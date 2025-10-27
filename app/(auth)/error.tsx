f489087b8faee62cceb2c0207b87e0b4b873ed02
Maycon Marques <mayconmarquesh@gmail.com>
2025-10-27 00:31:13 -0300

    error boundaries

---
+++ app/(auth)/error.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { useEffect } from "react"
+import { RefreshCcw } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className="space-y-4 text-center">
+      <h2 className="font-sans">{error.message}</h2>
+      <Button variant="secondary" onClick={() => reset()} className="mx-auto text-center">
+        Recarregar página <RefreshCcw />
+      </Button>
+    </div>
+  )
+}
